c8df3ae891d3cbfcec495b0188c5fb5f2c39c116
unistroy2010 <unistroy2010@yandex.ru>
2022-08-18 16:34:07 +0300

    Prog01

---
+++ examples/Prog01/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу которая на вход принимает два числа 
+и проверяет, является ли второе число квадратом первого
+*/
+
+Console.WriteLine("Введите число 1:");
+int UserNumber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Возможный квадрат");
+int UserNumber2 = Convert.ToInt32(Console.ReadLine());
+
+if (UserNumber2 == UserNumber1 * UserNumber1)
+{
+    Console.WriteLine("true");
+}
+else
+{
+   Console.WriteLine("false"); 
+}
